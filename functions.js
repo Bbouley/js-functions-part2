85ef1afccfb5e11200e16f26917c2fbf4f1c4496
bbouley <b.bouley@hotmail.co.uk>
2015-07-16 16:54:51 -0600

    finished up to question 3

--- functions.js
+++ functions.js
@@ -14,11 +14,12 @@ function oldestPerson (array) {
 }
 
 
-var longString = "this is a ridiculously long string"
+var longString = "this,?;== is, a-= ridi', long string"
 
 
 function longestWord(string){
-  var array = longString.split (" ");
+  var stringNoPunctuation = string.replace(/[^\w\s]|_/g, "");
+  var array = stringNoPunctuation.split (" ");
   var letterNumbers = '';
   for (i=0; i<array.length; i++){
     if (array[i].length > letterNumbers.length){
